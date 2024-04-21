2686e2bd93cd18f767332478d7d6047247becbe4
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-04-21 18:30:32 +0300

    castle in progress

--- scenes/levels/Castle/castle.gd
+++ scenes/levels/Castle/castle.gd
@@ -1,9 +1,9 @@
 extends Node3D
 
 func set_player_parameters(player):
-	player.gravity_scale = 1
+	#player.gravity_scale = 0.8
 	player.physics_material_override.bounce = 0.4
-	
+	pass
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
