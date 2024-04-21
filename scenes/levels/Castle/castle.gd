3bb76210d3c857a7aba4abd8da8f97c1429a13a6
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-04-21 20:08:56 +0300

    castle modelling done

--- scenes/levels/Castle/castle.gd
+++ scenes/levels/Castle/castle.gd
@@ -1,14 +1,17 @@
 extends Node3D
 
+@onready var pistonM1 = $castle/cyl_dyn_m1
+@onready var pistonM2 = $castle/cyl_dyn_m2
+@onready var pistonS1 = $castle/cyl_dyn_s1
+@onready var pistonS2 = $castle/cyl_dyn_s2
+
 func set_player_parameters(player):
-	#player.gravity_scale = 0.8
-	player.physics_material_override.bounce = 0.4
 	pass
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	print(pistonM1.position.z)
+	print(pistonM2.position.z)
 	pass
