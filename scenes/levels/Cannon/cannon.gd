a47282e02c63eac3451634d897684f0c84a4535a
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-04-24 12:10:12 +0300

    cannon level completed

--- scenes/levels/Cannon/cannon.gd
+++ scenes/levels/Cannon/cannon.gd
@@ -1,9 +1,13 @@
 extends Node3D
 
 func set_player_parameters(player):
-	player.physics_material_override.bounce = 0.5
+	#player.gravity_scale = 0.8
+	#player.physics_material_override.bounce = 0.5
 	pass
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AccelerationArea.set_vector(Vector3(0,0,-1))
+	$AccelerationArea.coef = 60000
 	$Gate.set_powerup(Globals.POWERUP.THUNDER)
--- scenes/levels/Castle/castle.gd
+++ scenes/levels/Castle/castle.gd
@@ -1,15 +1,9 @@
 extends Node3D
 
-@onready var pistonM1 = $castle/cyl_dyn_m1
-@onready var pistonM2 = $castle/cyl_dyn_m2
-@onready var pistonS1 = $castle/cyl_dyn_s1
-@onready var pistonS2 = $castle/cyl_dyn_s2
-
 func set_player_parameters(player):
 	pass
 
 func _ready():
-	$Pistons/SmallPiston1/AnimationPlayer.play("piston_animation")
 	pass # Replace with function body.
 
 func _physics_process(delta):
