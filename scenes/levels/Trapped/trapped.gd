2066e2596631094004c717632e3f882b9e6022b0
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-04-28 18:29:30 +0300

    shado removed

--- scenes/levels/Trapped/trapped.gd
+++ scenes/levels/Trapped/trapped.gd
@@ -3,12 +3,15 @@ extends Node3D
 @onready var barriers = $Barriers
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$trapped/light.cast_shadow = false
+	$trapped/light_001.cast_shadow = false
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	$trapped/light.rotate_y(+0.01)
+	
 
 func set_player_parameters(player):
 	pass
