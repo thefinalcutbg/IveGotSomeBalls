567e6dfd19477d43a673e172879a57cf54e141cf
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-04-30 14:02:20 +0300

    acceleration adjustment

--- scenes/levels/Castlecoaster/castlecoaster.gd
+++ scenes/levels/Castlecoaster/castlecoaster.gd
@@ -8,7 +8,7 @@ func set_player_parameters(player):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AccelerationArea.set_vector(Vector3(0,0,-1))
-	$AccelerationArea.coef = 5500
+	$AccelerationArea.coef = 5000
 	
 	pass # Replace with function body.
 
