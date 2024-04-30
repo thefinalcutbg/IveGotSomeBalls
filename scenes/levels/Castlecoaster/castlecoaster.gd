255209953a8fae909d523e32c24e15384a915b44
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-04-30 03:30:25 +0300

    castlecoaster in progress

--- scenes/levels/Castlecoaster/castlecoaster.gd
+++ scenes/levels/Castlecoaster/castlecoaster.gd
@@ -1,14 +1,14 @@
 extends Node3D
 
 func set_player_parameters(player):
-	player.gravity_scale = 1.5
+	player.gravity_scale = 1.2
 	player.physics_material_override.bounce = 0.4
 	#player.set_thunder_range(15)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AccelerationArea.set_vector(Vector3(-0,0,-1))
-	$AccelerationArea.coef = 15000
+	$AccelerationArea.set_vector(Vector3(0,0,-1))
+	$AccelerationArea.coef = 6000
 	pass # Replace with function body.
 
 
