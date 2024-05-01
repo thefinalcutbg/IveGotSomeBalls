b3eb83ad5a26428d6b0b22e8703d8a90047ae8c7
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-05-02 02:14:02 +0300

    city and chase lvl gameplay changes

--- scenes/levels/Chase/chase.gd
+++ scenes/levels/Chase/chase.gd
@@ -3,12 +3,12 @@ extends Node3D
 func _ready():
 	$Gate.set_powerup(Globals.POWERUP.JUMP)
 	$AccelerationArea.set_vector(Vector3(0,0,1))
-	$AccelerationArea.coef = 300
+	$AccelerationArea.coef = 350
 	#$AnimationPlayer.play("BarrierAnimation")
 	$chase/Funnel.mesh.surface_get_material(0).albedo_color = "ffe1005b"
 	
 func _physics_process(delta):
-	$Diamonds/Path3D/PathFollow3D.progress += 90.0*delta
+	$Diamonds/Path3D/PathFollow3D.progress += 85.0*delta
 	
 func set_player_parameters(player):
 	player.physics_material_override.bounce = 0.4
