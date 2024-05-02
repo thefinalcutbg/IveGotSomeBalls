e63e2c42f85db20f65f22abffb57e4269144177c
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-05-02 15:35:32 +0300

    base wing color change

--- scenes/levels/Bounce/bounce.gd
+++ scenes/levels/Bounce/bounce.gd
@@ -6,6 +6,10 @@ func set_player_parameters(player):
 	$bounce/Wing1/StaticBody3D.constant_angular_velocity.x = -2.1
 	$bounce/Wing2/StaticBody3D.constant_angular_velocity.z = -2.1
 	$bounce/Wing3/StaticBody3D.constant_angular_velocity.x = 2.1
+	
+	$bounce/Wing1.mesh.surface_get_material(0).albedo_color = "ff7070"
+	$bounce/Wing2.mesh.surface_get_material(0).albedo_color = "ff7070"
+	$bounce/Wing3.mesh.surface_get_material(0).albedo_color = "ff7070"
 
 
 func _physics_process(delta):
