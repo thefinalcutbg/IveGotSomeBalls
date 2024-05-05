dc830233d3274ddfa36330622678398e655c210c
Hristo Konstantinov <hristo.konstantinov89@gmail.com>
2024-05-05 17:54:59 +0300

    seb

--- scenes/levels/Chase/chase.gd
+++ scenes/levels/Chase/chase.gd
@@ -4,8 +4,6 @@ func _ready():
 	$Gate.set_powerup(Globals.POWERUP.JUMP)
 	$AccelerationArea.set_vector(Vector3(0,-0.4,1))
 	$AccelerationArea.coef = 450
-	#$AnimationPlayer.play("BarrierAnimation")
-	$chase/Funnel.mesh.surface_get_material(0).albedo_color = "ffe1005b"
 	
 func _physics_process(delta):
 	$Diamonds/Path3D/PathFollow3D.progress += 85.0*delta
